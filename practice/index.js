455e75c4e2c982699060d3647c8119f8fe46a8ab
sosally <thdud4143@gmail.com>
2023-08-14 14:39:14 +0900

    일기장3

---
+++ practice/index.js
@@ -0,0 +1,21 @@
+let str1 = 'hello wo';
+console.log(str1.length);
+console.log(str1.charAt(1));
+console.log(str1.split(' '))
+
+let fruit = ['사과', '배', '포도'];
+console.log(fruit.length);
+fruit.push('딸기');
+fruit.pop();
+console.log(fruit);
+
+console.log(10+10);
+console.log(20%3)
+
+console.log('10'+'40')
+//플러스만 문자로 계산
+
+console.log(10=='10' && 20 == 10);
+console.log(10===10 || 20==15);
+
+console.log(10>20);
