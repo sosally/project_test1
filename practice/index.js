2366ebf48d9d2c8188739af6aae013ccb07a91f1
sosally <thdud4143@gmail.com>
2023-08-15 23:03:21 +0900

    0815

--- practice/index.js
+++ practice/index.js
@@ -1,21 +1,36 @@
-let str1 = 'hello wo';
-console.log(str1.length);
-console.log(str1.charAt(1));
-console.log(str1.split(' '))
 
-let fruit = ['사과', '배', '포도'];
-console.log(fruit.length);
-fruit.push('딸기');
-fruit.pop();
-console.log(fruit);
 
-console.log(10+10);
-console.log(20%3)
+function greetings(){
+    console.log('hello');
+    console.log('안녕');
+}
+greetings();
+greetings();
 
-console.log('10'+'40')
-//플러스만 문자로 계산
+function hello(name){
+    console.log('안녕하세요 '+name+'님!');
+};
 
-console.log(10=='10' && 20 == 10);
-console.log(10===10 || 20==15);
+hello('소영');
 
-console.log(10>20);
+function sing(singer, song){
+    console.log(`제가 좋아하는 가수는 ${singer}입니다.`);
+    console.log(`제가 좋아하는 곡은 ${song}입니다.`)
+};
+sing('','long way');
+
+
+function star (x, y){
+    console.log(x*y);
+}
+star(3,2);
+function go(number){
+    return number * 2;
+}
+console.log(go(2));
+function min(number){
+    return number * 2;
+}
+let x = min(2);
+let y = min(4);
+console.log(x*y);
