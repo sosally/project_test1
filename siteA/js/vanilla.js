869979a5d555d5a7c851592b2b2f4d76b7308568
sosally <thdud4143@gmail.com>
2023-08-21 13:10:12 +0900

    0821_2

---
+++ siteA/js/vanilla.js
@@ -0,0 +1,13 @@
+        //마우스 호버시 투뎁스 보이게하는..
+        const depth1=document.querySelector('.depth1');
+        depth1.addEventListener('mouseover',function(){
+            depth1.querySelectorAll('.depth2').forEach((o)=>{o.style.display='block'})
+            document.querySelector('header').classList.add('on');
+        });
+        // document.querySelector -html 선택
+        // addEventListener -화면에서 일어나는 동작에 대한 일을 시킴
+        //function -할일
+        depth1.addEventListener('mouseout',function(){
+            depth1.querySelectorAll('.depth2').forEach((o)=>{o.style.display='none'})
+            document.querySelector('header').classList.remove('on');
+        });
