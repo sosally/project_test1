e5d9778e9c9d14a28ca62b19c60bcb7d02db687f
sosally <thdud4143@gmail.com>
2023-09-01 15:55:17 +0900

    0901_1

---
+++ javascript/01/01.js
@@ -0,0 +1,10 @@
+var heading = document.querySelector('#heading');
+heading.onclick = function(){
+    heading.style.color = 'pink';
+    heading.style.backgroundColor = 'yellow';
+    // 복합문자 -> 카멜케이스로 작성
+}
+var text = document.querySelector('#text');
+text.onclick = function(){
+    text.style.color = 'blue';
+}
